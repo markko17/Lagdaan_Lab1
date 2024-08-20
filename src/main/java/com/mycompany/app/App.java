fb648606b207442445bd6a38ae235a6d846c5563
Mark Lagdaan <102361739+markko17@users.noreply.github.com>
2024-08-20 17:39:22 +0800

    Update App.java
    a little change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,44 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+package HalfDiamond;
+import java.util.Scanner;
+public class HalfDiamondPattern; {
 
-package com.mycompany.app;
-
-public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        Scanner s = new Scanner (System.in);
+        int n = s.nextInt();
+        System.out.println("*");
+        //first half
+        int i = 1;
+        while (i<=n) {
+            System.out.println("*");
+            int = j=1;
+            while (j<=i) { 
+                System.out.println(j);
+                j=j+1;
+            }
+            j=j-2;
+            while (j>=1) {
+                
+                System.out.println(j);
+                j=j-1;
+            }
+            System.out.println("*");
+            System.out.println();
+            i=i+1;
+        }
+        //second half
+        i=1;
+        while (i<=n-1) {
+            System.out.println(j);
+            j=j+1;
+        }
+        j=j-2;
+        while (j>=1) {
+            System.out.println(j);
+            j=j-1;
+        }
+        System.out.println("*");
+        System.out.println();
+        i=i+1;
     }
+    System.out.println("*");
 }
